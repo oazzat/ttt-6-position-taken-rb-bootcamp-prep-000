fa34d54b98966886caee4a05d7df831189bdbaa1
Omar A <oazzat19@gmail.com>
2018-09-16 00:33:30 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,11 @@
 # code your #position_taken? method here!
 def position_taken? (board, ind)
   
-  if board[ind] == " " || nil
+  if board[ind] == " "
+    false
+  elsif board[ind] == ""
+    false
+  elsif board[ind] == nil
     false
   elsif board[ind] == "X" || "O"
     true
